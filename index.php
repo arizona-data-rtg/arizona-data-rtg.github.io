6679cc7c42fdbb48425ae55b98456f6c53029cc2
K K Lin <klin@math.arizona.edu>
2020-10-10 09:35:08 -0700

    add index.html index.php

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+<title>UA RTG</title>
+<link type="text/css" rel="stylesheet" href="https://math.arizona.edu/~klin/rtg-seminar/style.css">
+</head>
+
+<body><div class="main">
+
+<!-- PICTURE, CONTACT, etc -->
+
+  <h1>NSF Research Training Group in Data Driven Discovery</h1>
+
+      <?php echo file_get_contents("menu.html"); ?>
+
+  <center>
+    <table width=640>
+      <tr valign=center>
+	<td>
+	  Welcome to
+	  UA's <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1937229&HistoricalAwards=false">NSF-supported</a>
+	  Research Training Group (RTG) in the Data Driven Discovery!
+	  Hosted by
+	  the <a href="http://cos.arizona.edu/content/school-of-mathematical-sciences">School
+	  of Mathematical Sciences</a> at
+	  the <a href="arizona.edu">University of Arizona</a>, we are a
+	  group of <a href="people.php">faculty, postdocs, and
+	  students</a> (graduate and undergraduate) interested in modern
+	  computational methods for data driven modeling and their
+	  application to physical, biological, and engineered
+	  systems.<p>
+
+	    The RTG Seminar covers a range of topics, including
+	    expository talks, discussions of interesting papers, and the
+	    occasional formal seminar talk.  The seminar welcomes
+	    participation from all members of the mathematical sciences
+	    community at the University of Arizona.<p>
+
+	</td>
+
+	<!-- spacing -->
+	<td><table width=24><tr><td></td></tr></table></td>
+
+	<td>
+	  <table width=250>
+	    <tr><td>
+		<strong>Fall 2020:</strong> Tuesdays 2pm on Zoom<br>
+		<strong>Organizer:</strong>
+		<a href="http://math.arizona.edu/~klin">Kevin K
+		  Lin</a><p>
+
+	    </td></tr>
+	      <tr align=left>
+	        <td>
+		  <img src="https://math.arizona.edu/~klin/images/nsf.png"
+		       style="background-color:white;">
+	          <img src="https://math.arizona.edu/~klin/images/ua.png"
+		       style="background-color:white;">
+		</td>
+	        </tr>
+	  </table>
+	</td>
+      </tr>
+    </table>
+  </center><p>
+
+
+
+      <!-- If you wish to participate, please send e-mail to one of us to get -->
+      <!-- added to -->
+      <!-- the <a href="https://groups.google.com/a/math.arizona.edu/d/forum/uqg">group</a>. -->
+      <!-- You can also check this page for the <a href="#cal">calendar of -->
+      <!-- events</a> and the -->
+      <!-- <a href="speakers.php">list of talks</a>.<p> -->
+
+<div class=sect><p>
+<a name="cal">
+
+<iframe src="https://calendar.google.com/calendar/embed?src=c_dsrp52f34h5pgm3u3ifcsh3sqo%40group.calendar.google.com&ctz=America%2FPhoenix" style="border: 0" width="700" height="600" frameborder="0" scrolling="no"></iframe>
+
+</div>
+
+
+<address>
+  <!--<?php echo file_get_contents("cc.html"); ?>-->
+
+  This page was last updated on <?php echo date("F d, Y",
+  getlastmod()); ?>.<p>
+
+</address>
+
+</div></body>
+</html>
